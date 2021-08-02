6c3bda0fda54c8bf43e3c71a81c5510b9322d842
Daniel Kirwan <danielkirwan07@gmail.com>
2021-08-02 15:40:58 +0100

    Added run and idle 3D animations

---
+++ 3D Platformer/Assets/GameDevHQ/FileBase/Projects/Tutorials/Starter_Files/2_5D_Puzzle_Starter/Scripts/PlayerController.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private CharacterController _controller;
+    [SerializeField]
+    private float _speed = 5.0f;
+    [SerializeField]
+    private float _gravity = 1.0f;
+    [SerializeField]
+    private float _jumpHeight = 15.0f;
+    private float _yVelocity;
+    private Vector3 _direction, _velocity;
+    private bool _canDoubleJump = false;
+    [SerializeField] Animator _anim;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMovement();
+    }
+
+    void PlayerMovement()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        _anim.SetFloat("Speed", Mathf.Abs(horizontalInput));
+
+        if (_controller.isGrounded == true)
+        {
+            _direction = new Vector3(0, 0, horizontalInput);
+            _velocity = _direction * _speed;
+            //_wallJump = false;
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                _yVelocity = _jumpHeight;
+                _canDoubleJump = true;
+            }
+        }
+        else
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                if (_canDoubleJump == true)
+                {
+                    _yVelocity += _jumpHeight;
+                    _canDoubleJump = false;
+                }
+            }
+            _yVelocity -= _gravity;
+        }
+        _velocity.y = _yVelocity;
+
+        _controller.Move(_velocity * Time.deltaTime);
+    }
+
+}
