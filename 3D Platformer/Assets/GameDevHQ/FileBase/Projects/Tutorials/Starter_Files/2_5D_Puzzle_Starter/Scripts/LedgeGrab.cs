0ef8d2727ca58c08b8c787447264460b8085c0c5
Daniel Kirwan <danielkirwan07@gmail.com>
2021-08-20 14:43:50 +0100

    Modular platform feature complete

--- 3D Platformer/Assets/GameDevHQ/FileBase/Projects/Tutorials/Starter_Files/2_5D_Puzzle_Starter/Scripts/LedgeGrab.cs
+++ 3D Platformer/Assets/GameDevHQ/FileBase/Projects/Tutorials/Starter_Files/2_5D_Puzzle_Starter/Scripts/LedgeGrab.cs
@@ -4,20 +4,28 @@ using UnityEngine;
 
 public class LedgeGrab : MonoBehaviour
 {
-    [SerializeField] private Vector3 _handPosition, _standPosition;
+    //[SerializeField] private Vector3 _handPosition, _standPosition;
 
+    [SerializeField] private Transform _hand, _stand;
+
+
+    private void Start()
+    {
+        //_handPosition = _hand.transform.position;
+        //_standPosition = _stand.transform.position;
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Ledge"))
         {
             PlayerController player = other.transform.parent.GetComponent<PlayerController>();
-            player.GrabbingLedge(_handPosition, this);
+            player.GrabbingLedge(_hand.transform.position, this);
         }
     }
 
     //returns the standing position for this ledge
     public Vector3 GetStandingPosition()
     {
-        return _standPosition;
+        return _stand.transform.position;
     }
 }
